513abdada9e0d81c574b9cfa9c0ae38ae1d9f06d
Meera Patel <patelmeeray@gmail.com>
2020-09-19 02:35:08 -0400

    automated data refresh

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,8 +7,10 @@ CONCEPT_RELATIONSHIP <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidp
 CONCEPT_SYNONYM <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/CONCEPT_SYNONYM.csv')
 LINKS_TO_RESOURCES <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/LINKS_TO_RESOURCES.csv')
 NAMES_AND_SYNONYMS <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/NAMES_AND_SYNONYMS.csv')
+OLD_REGISTRY_NUMBER_LOG <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/OLD_REGISTRY_NUMBER_LOG.csv')
+OLD_RN_URL_VALIDITY <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/OLD_RN_URL_VALIDITY.csv')
 REGISTRY_NUMBER_LOG <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/REGISTRY_NUMBER_LOG.csv')
 REGISTRY_NUMBERS <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/REGISTRY_NUMBERS.csv')
 RN_URL_VALIDITY <- broca::simply_read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/RN_URL_VALIDITY.csv')
 
-usethis::use_data(CLASSIFICATION,CONCEPT,CONCEPT_ANCESTOR,CONCEPT_RELATIONSHIP,CONCEPT_SYNONYM,LINKS_TO_RESOURCES,NAMES_AND_SYNONYMS,REGISTRY_NUMBER_LOG,REGISTRY_NUMBERS,RN_URL_VALIDITY, overwrite = TRUE)
+usethis::use_data(CLASSIFICATION,CONCEPT,CONCEPT_ANCESTOR,CONCEPT_RELATIONSHIP,CONCEPT_SYNONYM,LINKS_TO_RESOURCES,NAMES_AND_SYNONYMS,OLD_REGISTRY_NUMBER_LOG,OLD_RN_URL_VALIDITY,REGISTRY_NUMBER_LOG,REGISTRY_NUMBERS,RN_URL_VALIDITY, overwrite = TRUE)
