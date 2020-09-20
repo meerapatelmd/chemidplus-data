d0aba580a1b923a4cd8f9b34eeaf58b6d2392500
Meera Patel <patelmeeray@gmail.com>
2020-09-19 21:17:37 -0400

    automated data refresh

--- R/data.R
+++ R/data.R
@@ -90,7 +90,7 @@
 
 #' @title REGISTRY_NUMBER_LOG
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 8867 rows and 11 variables:
+#' @format A data frame with 8869 rows and 11 variables:
 #' \describe{
 #'   \item{\code{rnl_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{raw_concept}}{character COLUMN_DESCRIPTION}
@@ -121,7 +121,7 @@
 
 #' @title RN_URL_VALIDITY
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 2356 rows and 3 variables:
+#' @format A data frame with 2357 rows and 3 variables:
 #' \describe{
 #'   \item{\code{rnuv_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{rn_url}}{character COLUMN_DESCRIPTION}
