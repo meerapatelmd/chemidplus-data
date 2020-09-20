7b50063569c0d358925be7741826b2b3516cf0ab
Meera Patel <patelmeeray@gmail.com>
2020-09-19 20:34:51 -0400

    automated data refresh

--- R/data.R
+++ R/data.R
@@ -90,7 +90,7 @@
 
 #' @title REGISTRY_NUMBER_LOG
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 8845 rows and 11 variables:
+#' @format A data frame with 8846 rows and 11 variables:
 #' \describe{
 #'   \item{\code{rnl_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{raw_concept}}{character COLUMN_DESCRIPTION}
