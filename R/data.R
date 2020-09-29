30ef32c6ea737fe61f9dd684eb9398aad4c301c5
Meera Patel <patelmeeray@gmail.com>
2020-09-29 05:02:59 -0400

    update data package

--- R/data.R
+++ R/data.R
@@ -11,17 +11,17 @@
 
 #' @title CONCEPT
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 6799 rows and 10 variables:
+#' @format A data frame with 8441 rows and 10 variables:
 #' \describe{
-#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id}}{integer COLUMN_DESCRIPTION}
 #'   \item{\code{concept_name}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{domain_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{vocabulary_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_class_id}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{standard_concept}}{character COLUMN_DESCRIPTION}
 #'   \item{\code{concept_code}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_start_date}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_end_date}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{valid_start_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{valid_end_date}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{invalid_reason}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
@@ -31,10 +31,10 @@
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 14840 rows and 4 variables:
 #' \describe{
-#'   \item{\code{ancestor_concept_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{descendant_concept_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{min_levels_of_separation}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{max_levels_of_separation}}{character COLUMN_DESCRIPTION} 
+#'   \item{\code{ancestor_concept_id}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{descendant_concept_id}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{min_levels_of_separation}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{max_levels_of_separation}}{integer COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "CONCEPT_ANCESTOR"
@@ -43,11 +43,11 @@
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 47 rows and 6 variables:
 #' \describe{
-#'   \item{\code{concept_id_1}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{concept_id_2}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id_1}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id_2}}{integer COLUMN_DESCRIPTION}
 #'   \item{\code{relationship_id}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_start_date}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{valid_end_date}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{valid_start_date}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{valid_end_date}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{invalid_reason}}{character COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
@@ -57,9 +57,9 @@
 #' @description DATASET_DESCRIPTION
 #' @format A data frame with 153366 rows and 3 variables:
 #' \describe{
-#'   \item{\code{concept_id}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{concept_id}}{integer COLUMN_DESCRIPTION}
 #'   \item{\code{concept_synonym_name}}{character COLUMN_DESCRIPTION}
-#'   \item{\code{language_concept_id}}{character COLUMN_DESCRIPTION} 
+#'   \item{\code{language_concept_id}}{integer COLUMN_DESCRIPTION} 
 #'}
 #' @details DETAILS
 "CONCEPT_SYNONYM"
@@ -102,7 +102,7 @@
 
 #' @title REGISTRY_NUMBER_LOG
 #' @description DATASET_DESCRIPTION
-#' @format A data frame with 9728 rows and 11 variables:
+#' @format A data frame with 9734 rows and 11 variables:
 #' \describe{
 #'   \item{\code{rnl_datetime}}{double COLUMN_DESCRIPTION}
 #'   \item{\code{raw_concept}}{character COLUMN_DESCRIPTION}
