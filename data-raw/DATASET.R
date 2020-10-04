16e8566844be7625dd632be7d890cebe6eb69d50
Meera <patelmeeray@gmail.com>
2020-10-04 00:58:47 -0400

    consolidated all chemidplus data into single repo

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,10 +1,10 @@
 library(readr)
-CLASSIFICATION <- readr::read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/CLASSIFICATION.csv')
-LINKS_TO_RESOURCES <- readr::read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/LINKS_TO_RESOURCES.csv')
-NAMES_AND_SYNONYMS <- readr::read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/NAMES_AND_SYNONYMS.csv')
-REGISTRY_NUMBER_LOG <- readr::read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/REGISTRY_NUMBER_LOG.csv')
-REGISTRY_NUMBERS <- readr::read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/REGISTRY_NUMBERS.csv')
-RN_URL_VALIDITY <- readr::read_csv('/Users/meerapatel/GitHub/chemidplusData/data-raw/RN_URL_VALIDITY.csv')
+CLASSIFICATION <- readr::read_csv('data-raw/CLASSIFICATION.csv')
+LINKS_TO_RESOURCES <- readr::read_csv('data-raw/LINKS_TO_RESOURCES.csv')
+NAMES_AND_SYNONYMS <- readr::read_csv('data-raw/NAMES_AND_SYNONYMS.csv')
+REGISTRY_NUMBER_LOG <- readr::read_csv('data-raw/REGISTRY_NUMBER_LOG.csv')
+REGISTRY_NUMBERS <- readr::read_csv('data-raw/REGISTRY_NUMBERS.csv')
+RN_URL_VALIDITY <- readr::read_csv('data-raw/RN_URL_VALIDITY.csv')
 
 usethis::use_data(
 	CLASSIFICATION,
