62134b0918f7152758ac1b0870bded631f233911
Meera <patelmeeray@gmail.com>
2020-10-12 16:00:40 -0400

    add ddl sql; backup local data

---
+++ inst/ddl.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS chemidplus.classification;
+CREATE TABLE chemidplus.classification (
+    c_datetime timestamp without time zone,
+    concept_classification character varying(255),
+    rn_url character varying(255)
+);
+
+DROP TABLE IF EXISTS chemidplus.links_to_resources;
+CREATE TABLE chemidplus.links_to_resources (
+    ltr_datetime timestamp without time zone,
+    resource_agency character varying(255),
+    resource_link character varying(255),
+    rn_url character varying(255)
+);
+
+DROP TABLE IF EXISTS chemidplus.names_and_synonyms;
+CREATE TABLE chemidplus.names_and_synonyms (
+    nas_datetime timestamp without time zone,
+    rn_url character varying(255),
+    concept_synonym_type character varying(255),
+    concept_synonym_name character varying(255)
+);
+
+DROP TABLE IF EXISTS chemidplus.registry_number_log;
+CREATE TABLE chemidplus.registry_number_log (
+    rnl_datetime timestamp without time zone,
+    raw_concept character varying(255),
+    processed_concept character varying(255),
+    type character varying(255),
+    url character varying(255),
+    response_received character varying(255),
+    no_record character varying(255),
+    response_recorded character varying(255),
+    compound_match character varying(255),
+    rn character varying(255),
+    rn_url character varying(255)
+);
+
+DROP TABLE IF EXISTS chemidplus.registry_numbers;
+CREATE TABLE chemidplus.registry_numbers (
+    rn_datetime timestamp without time zone,
+    rn_url character varying(255),
+    concept_registry_number_type character varying(255),
+    concept_registry_number character varying(255)
+);
+
+DROP TABLE IF EXISTS chemidplus.rn_url_validity;
+CREATE TABLE chemidplus.rn_url_validity (
+    rnuv_datetime timestamp without time zone,
+    rn_url character varying(255),
+    is_404 character varying(255)
+);
