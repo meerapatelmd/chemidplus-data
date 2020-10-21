f0694d1ca0caf649011bd73cb1de78299d0d73c1
Meera <patelmeeray@gmail.com>
2020-10-21 17:42:21 -0400

    update data package

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,17 +1,10 @@
 library(readr)
-library(dplyr)
-CLASSIFICATION <- readr::read_csv('data-raw/CLASSIFICATION.csv') %>%
-                        dplyr::mutate_if(is.character,  ~stringr::str_remove_all(., "[^ -~]"))
-LINKS_TO_RESOURCES <- readr::read_csv('data-raw/LINKS_TO_RESOURCES.csv') %>%
-                        dplyr::mutate_if(is.character,  ~stringr::str_remove_all(., "[^ -~]"))
-NAMES_AND_SYNONYMS <- readr::read_csv('data-raw/NAMES_AND_SYNONYMS.csv')   %>%
-                                dplyr::mutate_if(is.character,  ~stringr::str_remove_all(., "[^ -~]"))
-REGISTRY_NUMBER_LOG <- readr::read_csv('data-raw/REGISTRY_NUMBER_LOG.csv')   %>%
-                                dplyr::mutate_if(is.character,  ~stringr::str_remove_all(., "[^ -~]"))
-REGISTRY_NUMBERS <- readr::read_csv('data-raw/REGISTRY_NUMBERS.csv') %>%
-                                dplyr::mutate_if(is.character,  ~stringr::str_remove_all(., "[^ -~]"))
-RN_URL_VALIDITY <- readr::read_csv('data-raw/RN_URL_VALIDITY.csv')  %>%
-        dplyr::mutate_if(is.character,  ~stringr::str_remove_all(., "[^ -~]"))
+CLASSIFICATION <- readr::read_csv('data-raw/CLASSIFICATION.csv')
+LINKS_TO_RESOURCES <- readr::read_csv('data-raw/LINKS_TO_RESOURCES.csv')
+NAMES_AND_SYNONYMS <- readr::read_csv('data-raw/NAMES_AND_SYNONYMS.csv')
+REGISTRY_NUMBER_LOG <- readr::read_csv('data-raw/REGISTRY_NUMBER_LOG.csv')
+REGISTRY_NUMBERS <- readr::read_csv('data-raw/REGISTRY_NUMBERS.csv')
+RN_URL_VALIDITY <- readr::read_csv('data-raw/RN_URL_VALIDITY.csv')
 
 usethis::use_data(
 	CLASSIFICATION,
